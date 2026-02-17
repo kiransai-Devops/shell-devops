cb2e55a4c4017b0457463eb5604bc59e10938030
kiransaiveerandi <kiransaiveerandi@gmail.com>
2026-02-17 21:01:58 +0530

    ec2

--- ec2route53.sh
+++ ec2route53.sh
@@ -5,7 +5,7 @@ SG_ID="sg-01534c2e766e1b8e9"
 
 for instance in $@
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --subnet-id subnet-086d349d9664ac24c --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     
     if [ $instance != "frontend" ]; then
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID  --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
